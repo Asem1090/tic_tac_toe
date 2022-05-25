d4a5411bc3d54fa83ea131b45556ab1ec9480dc1
Asem1090 <asem.sh2004@gmail.com>
2022-05-25 16:51:06 +0300

    25/5/2022 16:50

--- src/windows/processors/start_window_processor.py
+++ src/windows/processors/start_window_processor.py
@@ -9,6 +9,9 @@ class StartWindowProcessor:
         if window_controller is None:
             raise AttributeError("ControllersManager.start_window_controller is None")
 
-        self.__x_o_buttons = \
-            {f"b{i}": window_controller.start_window.findChild(QPushButton, f"b{i}") for i in range(1, 10)}
+        # self.__x_o_buttons = \
+        #     {f"b{i}": window_controller.start_window.findChild(QPushButton, f"b{i}") for i in range(1, 10)}
 
+        self.pvp_btn = window_controller.start_window.findChild(QPushButton, "pvp_btn")
+
+        print(self.pvp_btn.objectName())
